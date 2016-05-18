bfc525dad80015f6bcd12a76587a64b73785184b
Kyle Luck <luck.kyle@gmail.com>
2016-05-18 10:54:05 -0400

    adding animiation with animate.css

--- memory.js
+++ memory.js
@@ -5,20 +5,20 @@ var currElement;
 $(function() {
   $('.tile').click(function() {
     if (state) {
-      $(this).addClass('selected');
+      $(this).addClass('selected animated flipInY');
       prevElement = $(this);
       state = false;
     } else {
       currElement = $(this);
-      currElement.addClass('selected');
+      currElement.addClass('selected animated flipInY');
 
       //check if monsters match
       if (prevElement.find('.monster').attr('src') === currElement.find('.monster').attr('src')) {
         //do nothing
       } else {
         setTimeout(function() {
-          currElement.removeClass('selected');
-          prevElement.removeClass('selected');
+          currElement.removeClass('selected animated flipInY');
+          prevElement.removeClass('selected animated flipInY');
         }, 1000);
       }
       state = true;
