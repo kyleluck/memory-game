94a01d941154d2a1053a830975b24228ca83a637
Kyle Luck <luck.kyle@gmail.com>
2016-05-18 10:32:36 -0400

    leave tiles revealed if they match, hide otherwise

--- memory.js
+++ memory.js
@@ -11,10 +11,16 @@ $(function() {
     } else {
       currElement = $(this);
       currElement.addClass('selected');
-      setTimeout(function() {
-        currElement.removeClass('selected');
-        prevElement.removeClass('selected');
-      }, 1000);
+
+      //check if monsters match
+      if (prevElement.find('.monster').attr('src') === currElement.find('.monster').attr('src')) {
+        //do nothing
+      } else {
+        setTimeout(function() {
+          currElement.removeClass('selected');
+          prevElement.removeClass('selected');
+        }, 1000);
+      }
       state = true;
     }
   });
