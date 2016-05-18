4aef0e21af1dcdf4763c8ad75bd221216c3d2ee5
Kyle Luck <luck.kyle@gmail.com>
2016-05-18 10:12:06 -0400

    initial logic to show/hide tiles

--- memory.js
+++ memory.js
@@ -1,3 +1,21 @@
-$(function() {
+var state = true;
+var prevElement;
+var currElement;
 
+$(function() {
+  $('.tile').click(function() {
+    if (state) {
+      $(this).addClass('selected');
+      prevElement = $(this);
+      state = false;
+    } else {
+      currElement = $(this);
+      currElement.addClass('selected');
+      setTimeout(function() {
+        currElement.removeClass('selected');
+        prevElement.removeClass('selected');
+      }, 1000);
+      state = true;
+    }
+  });
 });
