5cf138af37662387201531312fcbe2df339f42cc
Kyle Luck <luck.kyle@gmail.com>
2016-05-18 13:07:16 -0400

    fix bug if user clicks on same tile twice

--- memory.js
+++ memory.js
@@ -22,19 +22,25 @@ $(function() {
       state = false;
     } else {
       currElement = $(this);
-      currElement.addClass('selected animated flipInY');
-
-      //check if monsters match
-      if (prevElement.find('.monster').attr('src') === currElement.find('.monster').attr('src')) {
-        //do nothing
-      } else {
-        setTimeout(function() {
-          currElement.removeClass('selected animated flipInY');
-          prevElement.removeClass('selected animated flipInY');
-        }, 1000);
+      //check to see if the user clicked on the same tile... stupid users
+      if (currElement.hasClass('selected')) {
+        state = false;
       }
-      state = true;
-      numMoves++;
+      else {
+        currElement.addClass('selected animated flipInY');
+
+        //check if monsters match
+        if (prevElement.find('.monster').attr('src') === currElement.find('.monster').attr('src')) {
+          //do nothing
+        } else {
+          setTimeout(function() {
+            currElement.removeClass('selected animated flipInY');
+            prevElement.removeClass('selected animated flipInY');
+          }, 1000);
+        }
+        state = true;
+        numMoves++;
+      }
     }
     checkIfGameOver(numMoves);
   });
